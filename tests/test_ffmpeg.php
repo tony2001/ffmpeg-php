444a9b750d2d4f5106e726c19aeaeb51f91fb3aa
tkirby <tkirby@f31480fa-141b-0410-8f85-e1b837e7f62b>
2004-10-26 15:19:55 +0000

    format output with <pre></pre> when called from mod_php
    git-svn-id: https://ffmpeg-php.svn.sourceforge.net/svnroot/ffmpeg-php/trunk/ffmpeg-php@5 f31480fa-141b-0410-8f85-e1b837e7f62b

--- tests/test_ffmpeg.php
+++ tests/test_ffmpeg.php
@@ -2,21 +2,31 @@
 $module = 'ffmpeg';
 
 // load extension
-if(!extension_loaded($module))
+if(!extension_loaded($module)) {
     dl($module . "." . PHP_SHLIB_SUFFIX);
+}
 
-if (!extension_loaded($module))
+if (!extension_loaded($module)) {
     die("Module $module is not compiled into PHP");
+}
+
+// check to see if run buy php cgi
+$cgi = ((substr(php_sapi_name(), 0, 3) == 'cgi')) ? true : false;
+
+if (!$cgi) {
+    echo '<pre>';
+}
 
 // print available functions
 $functions = get_extension_funcs($module);
 echo "Functions available in the $module extension:<br>\n";
 
-foreach($functions as $func)
-    echo $func."<br>\n";
+foreach($functions as $func) {
+    echo $func."\n";
+}
 
 // put some movie files into this array to test the ffmpeg functions
-$movies = array( /*"/home/tkirby/movies/cowbell.wmv", */);
+$movies = array( "/home/tkirby/movies/cowbell.wmv" );
 
 echo "--------------------\n\n";
 foreach($movies as $movie) {        
@@ -29,7 +39,12 @@ foreach($movies as $movie) {
     printf("title = %s\n", $mov->getTitle());
     printf("author = %s\n", $mov->getAuthor());
     printf("copyright = %s\n", $mov->getCopyright());
-// BROKEN    $mov->getFrame(1);
+    // BROKEN    $mov->getFrame(1);
     echo "\n--------------------\n\n";
 }
+
+if (!$cgi) {
+    echo '</pre>';
+}
+
 ?>
