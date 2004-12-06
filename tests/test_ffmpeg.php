d2387ac246ff901f90afe9d13596ba031c6d9f1c
tkirby <tkirby@f31480fa-141b-0410-8f85-e1b837e7f62b>
2004-12-06 09:07:03 +0000

    Fixed up the dl()ing stuff a bit
    git-svn-id: https://ffmpeg-php.svn.sourceforge.net/svnroot/ffmpeg-php/trunk/ffmpeg-php@228 f31480fa-141b-0410-8f85-e1b837e7f62b

--- tests/test_ffmpeg.php
+++ tests/test_ffmpeg.php
@@ -8,9 +8,14 @@
  * browser * copy this file into your web root and point your browser at it.
  */
 
-$extension = 'ffmpeg';
-$extension_soname = $extension . '.' . PHP_SHLIB_SUFFIX;
-$extension_fullname = PHP_EXTENSION_DIR . '.' . $extension_soname;
+$extension = "ffmpeg";
+$extension_soname = $extension . "." . PHP_SHLIB_SUFFIX;
+$extension_fullname = PHP_EXTENSION_DIR . "/" . $extension_soname;
+
+// load extension
+if (!extension_loaded($extension)) {
+    dl($extension_soname) or die("Can't load extension $extension_fullname\n");
+}
 
 
 function getDirFiles($dirPath)
@@ -31,11 +36,6 @@ function getDirFiles($dirPath)
 
 if (php_sapi_name() != 'cgi') {
     echo '<pre>';
-}
-
-// load extension
-if(!extension_loaded($extension)) {
-    dl($extension_soname) or die("Can't load extension $extension_fullname\n");
 }
 
 // print available functions and aliases
