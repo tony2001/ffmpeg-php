0d0f28d1cd86c47b91ac3b693e8acccf942433b3
tkirby <tkirby@f31480fa-141b-0410-8f85-e1b837e7f62b>
2005-03-22 22:35:33 +0000

    Fixed pre tags
    git-svn-id: https://ffmpeg-php.svn.sourceforge.net/svnroot/ffmpeg-php/trunk/ffmpeg-php@348 f31480fa-141b-0410-8f85-e1b837e7f62b

--- tests/test_ffmpeg.php
+++ tests/test_ffmpeg.php
@@ -34,7 +34,7 @@ function getDirFiles($dirPath)
 }
 
 
-if (php_sapi_name() != 'cgi') {
+if (php_sapi_name() == 'cgi') {
     echo '<pre>';
 }
 
@@ -90,7 +90,7 @@ foreach($movies as $movie) {
     echo "\n--------------------\n\n";
 }
 
-if (php_sapi_name() != 'cgi') {
+if (php_sapi_name() == 'cgi') {
     echo '</pre>';
 }
 
