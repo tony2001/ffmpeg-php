9f37fe89e525963d9a95397ffa8d6715e8b2d40c
Andras Elso <elso.andras@gmail.com>
2014-02-20 13:05:53 +0000

    Set the frame number from right value

--- ffmpeg_movie.c
+++ ffmpeg_movie.c
@@ -1240,7 +1240,7 @@ static AVFrame* _php_get_av_frame(ff_movie_context *ffmovie_ctx, int wanted_fram
     while (1) {
         frame = _php_read_av_frame(ffmovie_ctx, decoder_ctx, is_keyframe, pts);
 
-        ffmovie_ctx->frame_number++;
+        ffmovie_ctx->frame_number = decoder_ctx->frame_number;
 
         /*
          * if caller wants next keyframe then get it and break out of loop.
